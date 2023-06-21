5417c2f4974356853c7a2f0fdadb6505f402afef
Ding <weibing.ding@digika.jp>
2023-06-21 23:11:34 +0900

    Init commit

---
+++ src/main/java/com/dingweibing/soro/check/SoroCheckApplication.java
@@ -0,0 +1,13 @@
+package com.dingweibing.soro.check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoroCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoroCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dingweibing/soro/check/SoroCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dingweibing.soro.check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoroCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
